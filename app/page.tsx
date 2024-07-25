8890593e18afd7d5c53ce3750177b9548ecc6899
Aleš Krejzl <ales.krejzl@gmail.com>
2024-07-25 21:21:26 +0200

    add magic button anim and json

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { navItems } from "@/data";
-import Image from "next/image";
 
-export default function Home() {
+
+const Home=()=> {
   return (
     <main className="relative flex flex-col justify-center items-center bg-black-100 overflow-hidden mx-auto px-5 sm:px-10 ">
       <div className="max-w-7xl w-full">
@@ -15,3 +15,4 @@ export default function Home() {
     </main>
   );
 }
+export default Home;
